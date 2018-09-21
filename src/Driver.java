13de82e8929492d486bcea2bb84297ca6004a0ce
Unknown <sshoemaker@mail.smu.edu>
2018-09-21 15:46:20 -0500

    Added TODOS

--- src/Driver.java
+++ src/Driver.java
@@ -4,15 +4,51 @@ import java.io.Console;
 
 import rxtxrobot.*;
 public class Driver {
+	private RXTXRobot robot;
+	private Console terminal;
 	public static void main(String[] args)
 	{
 		//RXTXRobot r = new ArduinoNano();
 		//reading input from user for program to run
 		Console terminal = System.console();
 		
+		terminal.printf("HELLO");
 		String input = terminal.readLine("Enter the program number ");
 		
 		System.out.println(input);
 		
 	}
+	
+	private void moveMotor(double meters)
+	{
+		//TODO
+	}
+	
+	private void moveServoMotor(double angle)
+	{
+		//TODO
+	}
+	
+	private char readIRChar()
+	{
+		//TODO
+		char output = '0';
+		
+		return output;
+	}
+	
+	private void MotorRunIndefinitely()
+	{
+		//TODO
+	}
+	
+	private double getTemperature()
+	{
+		//TODO
+		double output = -1;
+		return output;
+	}
+	
+	
+	
 }

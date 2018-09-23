a07dd64f02f15f37575d3dc56ac7c27784f1babf
Stephenshoemaker99 <sshoemaker@mail.smu.edu>
2018-09-22 21:15:26 -0500

    Adjusted some things in organization and added IR Beacon comman

--- src/Driver.java
+++ src/Driver.java
@@ -2,79 +2,48 @@
 import java.io.Console;
 import java.util.Scanner;
 
-import rxtxrobot.*;
+
 public class Driver {
 	private RXTXRobot robot;
 	private Console terminal;
 	public static void main(String[] args)
 	{
-		//RXTXRobot r = new ArduinoNano();
+		EXERobot robot = new EXERobot();
 		//reading input from user for program to run
 		Console terminal = System.console();
 		
 		terminal.printf("HELLO");
-		String input = terminal.readLine("Enter the program number ");
-		Scanner sc = new Scanner(System.in);
-		char in = sc.next()
-		System.out.println(input);
+		char in = terminal.readLine("Enter Program Number").charAt(0);
 
-		do
-		{
-			char in = sc.next()
+
+		while(in != 'q'){
 			switch(in)
 			{
 				case 'a':
-					moveMotor(0);//enter meters
+					EXERobot.moveMotor(0);//enter meters
 					break;
 				case 'b':
-					moveServoMotor(0);
+					EXERobot.moveServoMotor(0);
 					break;
 				case 'c':
-					System.out.println(readIRChar());
+					terminal.printf("" + EXERobot.readIRChar());
 					break;
 				case 'd':
-					motorRunIndefinitely();
+					EXERobot.motorRunIndefinitely();
 					break;
 				case 'e':
-					System.out.println(getTemperature());
+					terminal.printf("" + EXERobot.getTemperature());
 					break;
 			}
+			in = terminal.readLine("Enter Program Number").charAt(0);
 
 		}while(in != 'q');
 		
 	}
 	
-	private void moveMotor(double meters)
-	{
-		//TODO
-		//I swear to god if this does not work i am killing the man to my right
-	}
-	
-	private void moveServoMotor(double angle)
-	{
-		//TODO
-	}
-	
-	private char readIRChar()
-	{
-		//TODO
-		char output = '0';
-		
-		return output;
-	}
-	
-	private void motorRunIndefinitely()
-	{
-		//TODO
-	}
-	
-	private double getTemperature()
-	{
-		//TODO
-		double output = -1;
-		return output;
-	}
+
 	
 	
 	
 }
+

13ef7086456f5fb05237e28d8fbd2781a9781c94
Stephenshoemaker99 <sshoemaker@mail.smu.edu>
2018-09-26 18:58:59 -0500

    Added Constants to EXE ROBOT Channels

--- src/EXERobot.java
+++ src/EXERobot.java
@@ -3,41 +3,40 @@ public class EXERobot {
 
     //all the port constants should go here
     final private static int IR_SENSOR_PORT = 12;
+    final private static int MOTOR_LEFT = -1; //TODO
+    final private static int MOTOR_RIGHT = -1; //TODO
+    final private static int SERVO_MEASUREMENTS = -1;//TODO
+    final private static int SERVO_DROP_BALL = -1;//TODO
 
 
-    public EXERobot()
-    {
+    public EXERobot() {
         RXTXRobot robot = new ArduinoNano();
     }
 
-    public EXERobot(String port)
-    {
+    public EXERobot(String port) {
         RXTXRobot robot = new ArduinoNano();
         robot.setPort(port);
         robot.connect();
     }
 
 
-    private void moveMotor(int channel1, int speed1, int channel2, int speed2, int time)
-    {
+    private void moveMotor(int channel1, int speed1, int channel2, int speed2, int time) {
         //TODO
-        robot.runTwoPCAMotor(channel1, speed1, channel2, speed2, time);
+        robot.runTwoPCAMotor(MOTOR_LEFT, speed1, MOTOR_RIGHT, speed2, time);
 
     }
 
-    public static void moveServoMotor(double angle)
-    {
-        robot.runPCAServo(int channel, angle); // need to know the channel //TODO
+    public void moveServoMotor(double angle) {
+        robot.runPCAServo(SERVO_MEASUREMENTS, angle); // need to know the channel //TODO
     }
 
-    public static char readIRChar()
-    {
+    public char readIRChar() {
         //TODO
 
         robot.refreshDigitalPins();
         //pulling the IRChar multiple times to account for dead times
         for (int count = 0; count < 300; count++) {
-            System.out.println( robot.getPing(IR_SENSOR_PORT));
+            System.out.println(robot.getPing(IR_SENSOR_PORT));
             robot.sleep(300);
         }
 
@@ -47,11 +46,20 @@ public class EXERobot {
         return output;
     }
 
-    public static void motorRunIndefinitely()
-    {
-        robot.runPCAMotor(4, 50, 0);
-        //TODO
+    public void motorRunIndefinitely() {
+        boolean stopCondition; //create a function to check bump sensor
+        while (stopCondition) {
+            robot.runTwoPCAMotor(MOTOR_LEFT, speed1, MOTOR_RIGHT, speed2, 1);
+            stopConditon = checkBump();
+        }//TODO
     }
+
+    public boolean checkBump()
+    {
+        
+    }
+
+    //
 
     public static double getTemperature()
     {
